fd5885e348648f00e4f7bc0bc3a2898a8637a8e6
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-02-04 15:59:35 +0900

    Add test for codefix provider

---
+++ RoslynAnalyzerTemplate.Test/RoslynAnalyzerTemplateCodeFixProviderTest.cs
@@ -0,0 +1,44 @@
+// Copyright (c) 2020-2023 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Verify = Microsoft.CodeAnalysis.CSharp.Testing.NUnit.CodeFixVerifier<
+    RoslynAnalyzerTemplate.RoslynAnalyzerTemplate,
+    RoslynAnalyzerTemplate.RoslynAnalyzerTemplateCodeFixProvider>;
+
+namespace RoslynAnalyzerTemplate.Test;
+
+[TestFixture]
+public class RoslynAnalyzerTemplateCodeFixProviderTest
+{
+    /// <summary>
+    /// Test codefix provider for lowercase type name make uppercase
+    /// <seealso href="https://github.com/dotnet/roslyn-sdk/blob/main/src/Microsoft.CodeAnalysis.Testing/README.md"/>
+    /// </summary>
+    [Test]
+    public async Task TypeNameContainingLowercase_CodeFixed()
+    {
+        var source = ReadCodes("TypeNameContainingLowercase.cs");
+        var fixedSource = ReadCodes("TypeNameContainingLowercaseFixed.cs");
+        var expected = Verify.Diagnostic()
+            .WithSpan(13, 11, 13, 19)
+            .WithArguments("TypeName");
+
+        await Verify.VerifyCodeFixAsync(source, expected, fixedSource);
+    }
+
+    private static string ReadCodes(params string[] sources)
+    {
+        const string Path = "../../../TestData";
+        var stringBuilder = new StringBuilder();
+        foreach (var file in sources)
+        {
+            stringBuilder.Append(File.ReadAllText($"{Path}/{file}", Encoding.UTF8));
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ RoslynAnalyzerTemplate.Test/TestData/TypeNameContainingLowercaseFixed.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2020-2023 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TypeNameContainingLowercase
+{
+    class TYPENAME
+    {
+    }
+}
