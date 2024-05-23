16a96acdbd6b3c0b9b73357771851cb834f2b5e2
Yuki Sakai <yuukisakai123@gmail.com>
2024-05-23 23:49:29 +0900

    Add Plugins

---
+++ RoslynAnalyzerTemplate.Unity/Assets/Plugins/RoslynAnalyzerTemplate/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) 2020-2024 DeNA Co., Ltd.
+// This software is released under the MIT License.
+
+using System.Reflection;
+
+[assembly: AssemblyDescription(
+    "Suppress warning 'Assembly for Assembly Definition File will not be compiled, because it has no scripts associated with it.'")]
